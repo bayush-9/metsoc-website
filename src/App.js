c05bf04976ec83a890b7caf5ea4e6d3ac365f6f9
Ayush Anil Bharsakle <bharsakleaa@gmail.com>
2023-08-14 10:25:18 +0530

    Added top bar navigation skeleton

--- src/App.js
+++ src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <header className="top-nav">
+        <nav>
+          <ul className="nav-list">
+            <li><a href="/">Home</a></li>
+            <li><a href="/events">Events</a></li>
+            <li><a href="/teams">Teams</a></li>
+            <li><a href="/updates">Updates</a></li>
+            <li><a href="/gallery">Gallery</a></li>
+          </ul>
+        </nav>
       </header>
+      {/* Other content of your app */}
     </div>
   );
 }
