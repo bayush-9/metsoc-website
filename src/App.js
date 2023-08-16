18842371755eeb40f9b13c965320c73caccaa163
mpatwa98 <mandeep.patwa.met21@itbhu.ac.in>
2023-08-16 10:51:56 +0530

    card component implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Hero from './components/Hero';
-import Home from './components/Home';
-import Events from './components/Events';
-import Updates from './components/Updates';
-import Gallery from './components/Gallery';
-import Teams from './components/Teams';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Hero from "./components/Hero";
+import Home from "./components/Home";
+import Events from "./components/Events";
+import Updates from "./components/Updates";
+import Gallery from "./components/Gallery";
+import Teams from "./components/Teams";
 
 function App() {
-  return (
-    <Routes>
-      <Route index element={<Home />} />
-        <Route path="/" element={<Hero />} />
-        <Route path="/events" element={<Events/>} />
-        <Route path="/updates" element={<Updates/>} />
-        <Route path="/gallery" element={<Gallery/>} />
-        <Route path="/teams" element={<Teams/>} />
-       
-      </Routes>
-     
-   
-  );
+	return (
+		<Routes>
+			<Route index element={<Home />} />
+			<Route path="/" element={<Hero />} />
+			<Route path="/events" element={<Events />} />
+			<Route path="/updates" element={<Updates />} />
+			<Route path="/gallery" element={<Gallery />} />
+			<Route path="/teams" element={<Teams />} />
+		</Routes>
+	);
 }
 
 export default App;
