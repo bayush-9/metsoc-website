52331ed6c81614651b224c2a5436907a7aec1b88
Aparna <aparna.dubey.met21@itbhu.ac.in>
2023-09-25 18:13:27 +0530

    Edited Code for Updates Page

--- src/components/Updates.js
+++ src/components/Updates.js
@@ -141,20 +141,5 @@ const Updates = () => {
   </>
   )
 }
-=======
-import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-
-const Updates = () => {
-	return (
-		<>
-			<Navbar />
-			<p className="text-4xl bold text-center m-auto">Updates</p>
-			<Footer />
-		</>
-	);
-};
-
 
 export default Updates;
